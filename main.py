d25cb84a03f9c7ffe19fb8328743cb10530f5bd4
NamPham23 <152579684+NamPham23@users.noreply.github.com>
2023-12-08 09:19:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import streamlit as st 
+ 
+import streamlit as st
+from PIL import Image
 import pickle as pkl
 import numpy as np
 
-class_list ={'0': 'Negative', '1': 'Neutral','2': 'Positive'}
-st.title('Sentiment analysis from Vietnamese student feedback')
+class_list = {'0': 'Negative', '1': 'Positive', '2': 'Positive'}
 
-#image = Image.open('.png')
-#st.image(image)
+st.title('Sentiment analysis from Vietnamese students\' feedback')
 
-input_ec = open('ec_vsfc.pkl','rb')
+image = Image.open('feedback.jpg')
+st.image(image)
+
+input_ec = open('ec_vsfc.pkl', 'rb')
 encoder = pkl.load(input_ec)
 
-input_md = open('lrc_vsfc.pkl','rb')
+input_md = open('lrc_vsfc.pkl', 'rb')
 model = pkl.load(input_md)
 
-st.header('write a feedback')
-txt = st.text_area('','')
+st.header('Write a feedback')
+txt = st.text_area('', '')
 
 if txt != '':
   if st.button('Predict'):
     feature_vector = encoder.transform([txt])
     label = str((model.predict(feature_vector))[0])
-    
+
     st.header('Result')
-    st.text(class_list[lable])
-    
+    st.text(class_list[label])
