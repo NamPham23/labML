f03ff5fd5e4f258d957941919f1fe2f8763d19d2
NamPham23 <152579684+NamPham23@users.noreply.github.com>
2023-12-08 08:28:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import streamlit as st 
 import pickle as pkl
 import numpy as np
+
+class_list ={'0': 'Negative', '1': 'Neutral','2': 'Positive' }
+st.title('Sentiment analysis from Vietnamese student feedback')
+
+image = Image.open('.png')
+st.image(image)
+
+input_ec = open('ec_vsfc.pkl', 'rb')
+encoder = pkl.load(input_ec)
+
+input_md = open('lrc_vsfc.pkl', 'rb')
+model = pkl.load(input_md)
+
+st.header('write a feedback')
+txt = st.text_area('','')
+
+if txt != '':
+  if st.button('Predict'):
+    feature_vector = encoder.Transform([txt])
+    label = str((medel.predict(feature_vector))[0])
+    st.header('Result')
+    st.text(class_list[lable])
+    
